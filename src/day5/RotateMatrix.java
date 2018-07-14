0942cc674e885c93209d0476713b46feac4ed6af
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-14 13:23:43 -0700

    matrix is flipping but wrong way

--- src/day5/RotateMatrix.java
+++ src/day5/RotateMatrix.java
@@ -1,4 +1,22 @@
 package day5;
 
 public class RotateMatrix {
+
+
+     public static int[][]  Rotate(int n, int arr[][]) {
+
+        for(int row = 0; row < n / 2; row++){
+
+            for (int col = row; col < n-row-1; col++){
+
+                 int temp = arr[row][col];
+
+                 arr[row][col] = arr[col][n-1-row];
+                 arr[col][n-1-row] = arr[n-1-col][row];
+                 arr[n-1-col][row] = temp;
+            }
+        }
+
+        return arr;
+    }
 }
