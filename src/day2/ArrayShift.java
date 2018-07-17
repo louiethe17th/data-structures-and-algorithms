d7fa55ac0bd7fdcc84de14cc42f11c9ff9b4136d
Tyler Pearson <tylerthewonderfull@gmail.com>
2018-07-17 10:03:52 -0700

    fixed array shift

--- src/day2/ArrayShift.java
+++ src/day2/ArrayShift.java
@@ -1,28 +1,22 @@
 package day2;
 
-import java.util.Arrays;
 
 public class ArrayShift {
-    public static void main(String[] args) {
-        int[] firstArr = {2, 4, 6, 8};
-        int newVal = 5;
-        insertShiftArray(firstArr, newVal);
-    }
 
-    public static void insertShiftArray(int[] firstArr, int newVal) {
-        int length = firstArr.length;
+    public static int[] insertShiftArray(int[] arr, int newVal) {
+        int[] result = new int[arr.length + 1];
+        int midIndex = result.length / 2;
 
-        int[] tempA = new int[(length + 1) / 2];
-        int[] tempB = new int[length - tempA.length];
-
-        for (int i = 0; i < length; i++) {
-            if (i < tempA.length) {
-                tempA[i] = firstArr[i];
-            } else {
-                tempB[i - tempA.length] = firstArr[i];
-            }
-
-
+        for (int i = 0; i <= midIndex; i++) {
+            result[i] += arr[i];
         }
+        result[midIndex] = newVal;
+
+        for (int i = midIndex; i < arr.length; i++) {
+            result[i + 1] = arr[i];
+        }
+
+        return result;
     }
+
 }
